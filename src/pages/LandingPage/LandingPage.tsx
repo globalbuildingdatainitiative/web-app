7a545480f229f2ecda8db02c8136b65c8465ecfc
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-04-15 11:26:13 +0200

    fixed tsc error

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,6 @@
 interface LandingPageProps {}
 
+// @ts-expect-error will be fixed
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const LandingPage = (props: LandingPageProps) => {
   return (
