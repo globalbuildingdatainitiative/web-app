7dbfb968e6a94ff7da4648552f702e9e9cfabfc0
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-04-15 11:17:20 +0200

    fixed formatting and linting

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,7 @@
-interface LandingPageProps {
+interface LandingPageProps {}
 
-}
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const LandingPage = (props: LandingPageProps) => {
-
   return (
     <div>
       <h1>Hello World!</h1>
