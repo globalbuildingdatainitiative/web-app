2c5c2db0ac3dff7719444ca61916a3d715cc3371
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-05-13 13:10:16 +0200

    resolves #23 - Added functionality to upload SLiCE files as contributions

--- src/components/ContributionUploadPaper/ContributionUploadPaper.tsx
+++ src/components/ContributionUploadPaper/ContributionUploadPaper.tsx
@@ -1,43 +1,69 @@
 import { Paper } from '@components'
-import { Group, rem, Title, Text, Button } from '@mantine/core'
+import { Button, Group, rem, Stack, Text, Title } from '@mantine/core'
 import { Dropzone } from '@mantine/dropzone'
 import { IconPhoto, IconUpload, IconX } from '@tabler/icons-react'
 import { useState } from 'react'
-import { InputContribution, useAddContributionMutation } from '@queries'
+import { InputAssembly, InputContribution, InputProduct, InputProject, useAddContributionMutation } from '@queries'
 import { useNavigate } from 'react-router-dom'
+import { convertSLiCE } from 'lcax'
 
 export const ContributionUploadPaper = () => {
   const [addContributions, { loading, error }] = useAddContributionMutation({ refetchQueries: ['getContributions'] })
-  const [contributionData, setContributionData] = useState<InputContribution | null>(null)
+  const [contributionData, setContributionData] = useState<InputContribution[] | null>(null)
+  const [fileName, setFileName] = useState('')
+  const [fileLoading, setFileLoading] = useState(false)
   const navigate = useNavigate()
 
   const onContribute = async () => {
     if (!contributionData) {
       return
     }
-    await addContributions({ variables: { contributions: [contributionData] } })
+    const chunkSize = 10
+    if (contributionData.length > chunkSize) {
+      console.log(
+        `Attempting to upload ${contributionData.length} contributions. Will batch it to ${chunkSize} contributions per request.`,
+      )
+      for (let i = 0; i < contributionData.length; i += chunkSize) {
+        const contributionChunk = contributionData.slice(i, i + chunkSize)
+        await addContributions({ variables: { contributions: contributionChunk } })
+      }
+    } else {
+      await addContributions({ variables: { contributions: contributionData } })
+    }
+
     navigate('/contributions')
+  }
+
+  const processUploadedFile = async (files: File[]): Promise<InputContribution[]> => {
+    const file = files[0]
+    setFileName(file.name)
+
+    if (file.name.endsWith('.parquet')) {
+      setFileLoading(true)
+      const contributions = parseSLiCEtoContribution(new Uint8Array(await file.arrayBuffer()))
+      setFileLoading(false)
+      return contributions
+    }
+    return [{ project: { name: file.name } as InputProject }]
   }
 
   return (
     <Paper data-testid='ContributionUploadPaper'>
       <Title order={3}>Contribute Now</Title>
       <Text>In order to process the data correctly, please follow the steps below:</Text>
-      <Text>
-        <ol>
-          <li>Download the data template file here:</li>
-          <li>Paste your data as per template's format</li>
-          <li>Export the file as .xlsx</li>
-          <li>Upload the file below</li>
-        </ol>
-      </Text>
+      <Stack pl='md' py='md' gap={0}>
+        <Text>1. Download the data template file here:</Text>
+        <Text>2. Paste your data as per template's format</Text>
+        <Text>3. Export the file as .xlsx</Text>
+        <Text>4. Upload the file below</Text>
+      </Stack>
       <Dropzone
         data-testid='contributionDropzone'
         disabled={!!contributionData}
-        onDrop={(files) => setContributionData(processUploadedFile(files))}
+        loading={fileLoading}
+        onDrop={async (files) => setContributionData(await processUploadedFile(files))}
         onReject={(files) => console.log('rejected files', files)}
-        maxSize={5 * 1024 ** 2}
-        accept={['text/csv']}
+        maxSize={50 * 1024 ** 2}
         inputProps={{
           // @ts-expect-error data-testid is valid
           'data-testid': 'dropzoneInput',
@@ -70,7 +96,7 @@ export const ContributionUploadPaper = () => {
       {contributionData ? (
         <Group justify='flex-end' style={{ marginTop: 16 }}>
           {error ? <Text c='red'>{error.message}</Text> : null}
-          <Text>Upload {contributionData?.project.name}?</Text>
+          <Text>Upload {fileName}?</Text>
           <Button loading={loading} color='green' onClick={onContribute}>
             Contribute
           </Button>
@@ -80,7 +106,14 @@ export const ContributionUploadPaper = () => {
   )
 }
 
-const processUploadedFile = (files: File[]): InputContribution => {
-  const file = files[0]
-  return { project: { name: file.name } }
+const parseSLiCEtoContribution = (uint8Array: Uint8Array): InputContribution[] => {
+  const projects = convertSLiCE(uint8Array)
+
+  return projects.map((project) => {
+    const assemblies = Object.values(project.assemblies).map((assembly) => {
+      const products = Object.values(assembly.products) as InputProduct[]
+      return { ...assembly, products }
+    }) as InputAssembly[]
+    return { project: { ...project, assemblies } as InputProject }
+  })
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import wasm from 'vite-plugin-wasm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), wasm()],
   test: {
     globals: true,
     environment: 'happy-dom',
-    snapshotSerializers: ['src/__test__/snapshotSerializer.ts']
+    snapshotSerializers: ['src/__test__/snapshotSerializer.ts'],
+    alias: {
+      'lcax': `${__dirname}/src/__mocks__/lcax.ts`,
+    }
   },
 })
