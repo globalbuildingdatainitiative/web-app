7d7ad50cbd2c8b84c51eb7bedfb7a98fe6e8ace0
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-05-27 16:02:10 +0200

    Generic First Name Implemented

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,6 +1,10 @@
-import { AppShell, Button, Group, Stack, Title } from '@mantine/core'
-import { IconAffiliate, IconChevronRight, IconDashboard, IconUpload, IconUser } from '@tabler/icons-react'
+import {useState, useEffect} from 'react'
+import { AppShell, Button, Group, Stack, Title, Menu } from '@mantine/core'
+import { IconAffiliate, IconChevronRight, IconDashboard, IconLogout, IconUpload, IconUser } from '@tabler/icons-react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { signOut} from 'supertokens-auth-react/recipe/emailpassword'
+import {useGetUsersQuery} from '@queries'
+import { useSessionContext } from 'supertokens-auth-react/recipe/session'
 
 interface ButtonProps {
   name: string
@@ -12,6 +16,32 @@ export const SidePanel = () => {
   const location = useLocation()
   const navigate = useNavigate()
 
+  const [firstName, setFirstName] = useState('User')
+  const session = useSessionContext()
+  const {loading, error, data} = useGetUsersQuery()
+  const users = data?.users;
+  const currentUserID = session?.userId ?? ""
+
+  useEffect(() => {
+    async function fetchFirstName(){
+      if (session.loading || !session.doesSessionExist){
+        return
+      }
+
+      try {
+        if (users){
+          const user = users.find((user) => user.id === currentUserID)
+          if (user && user.firstName){
+            setFirstName(user.firstName)
+          }
+        }
+      } catch (error){
+        console.error('Failed to fetch user metadata:', error)
+      }
+    }
+    fetchFirstName()
+  }, [session, users])
+
   const buttons: ButtonProps[] = [
     { name: 'Dashboard', Logo: IconDashboard, link: '/' },
     { name: 'Contributions', Logo: IconUpload, link: '/contributions' },
@@ -20,6 +50,11 @@ export const SidePanel = () => {
   ]
 
   const currentPage = buttons.find(({ link }) => link === location.pathname) || buttons[0]
+
+  async function onLogout() {
+    await signOut()
+    navigate('/auth')
+  }
 
   return (
     <>
@@ -46,7 +81,21 @@ export const SidePanel = () => {
           ))}
         </Stack>
       </AppShell.Section>
-      <AppShell.Section>Hassan from EPFL</AppShell.Section>
+      <AppShell.Section style={{display:'flex', justifyContent: 'center', alignItems:'center'}}>
+        <Menu trigger='hover' openDelay={100} closeDelay={400} position='top' offset={8} withArrow arrowPosition='center'>
+          <Menu.Target>
+            <Button variant='outline' color='green' c='gray' size='md' radius='md'>
+              {firstName} from EPFL
+            </Button>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item onClick={onLogout}>
+              <IconLogout size={18} />
+              Sign Out
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </AppShell.Section>
     </>
   )
 }
