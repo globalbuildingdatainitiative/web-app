dc278371912e21ed94e63291dd48137d646b238c
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-05-29 13:49:21 +0200

    Closing #22

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,10 +1,38 @@
 import { Title } from '@mantine/core'
+import { useState, useEffect } from 'react'
+import { useGetUsersQuery } from '@queries'
+import { useSessionContext } from 'supertokens-auth-react/recipe/session'
 
 export const Greeting = () => {
-  const name = 'Hassan'
+  const [firstName, setFirstName] = useState('Unknown')
+  const session = useSessionContext()
+  const { data: usersData } = useGetUsersQuery()
+  const users = usersData?.users
+  useEffect(() => {
+    async function fetchFirstName() {
+      if (session.loading || !session.doesSessionExist) {
+        return
+      }
+
+      const currentUserID = session.userId
+
+      try {
+        if (currentUserID) {
+          const currentUser = users?.find((user) => user.id === currentUserID)
+          if (currentUser && currentUser.firstName) {
+            setFirstName(currentUser.firstName)
+          }
+        }
+      } catch (error) {
+        console.error('Failed to fetch user name:', error)
+      }
+    }
+    fetchFirstName()
+  }, [session, users])
+
   return (
     <Title order={3} style={{ paddingTop: 16 }}>
-      Hello {name} 👋🏼
+      Hello {firstName} 👋🏼
     </Title>
   )
 }
