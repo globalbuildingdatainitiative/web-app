330919f218e96219107419b2200368eb0c7f13ea
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-07-08 11:15:34 +0200

    User will now be redirected if not part of Organization

--- src/components/ContributionPaper/ContributionPaper.tsx
+++ src/components/ContributionPaper/ContributionPaper.tsx
@@ -1,23 +1,15 @@
-import { ContributionTable, CreateOrganizationPaper, ErrorBoundary, Loading, Paper } from '@components'
+import { ContributionTable, ErrorBoundary, Paper } from '@components'
 import { Title } from '@mantine/core'
-import { useUserContext, User } from '@context'
 
 export const ContributionPaper = () => {
-  const { user } = useUserContext()
   return (
     <Paper data-testid='ContributionPaper'>
       <ErrorBoundary>
         <Title order={3} style={{ marginBottom: 8 }}>
           All Contributions
         </Title>
-        <Wrapper user={user} />
+        <ContributionTable />
       </ErrorBoundary>
     </Paper>
   )
-}
-
-const Wrapper = ({ user }: { user: User | null }) => {
-  if (!user) return <Loading />
-  if (!user.organization) return <CreateOrganizationPaper />
-  return <ContributionTable />
 }
--- src/pages/ContributionPage/ContributionPage.tsx
+++ src/pages/ContributionPage/ContributionPage.tsx
@@ -1,7 +1,19 @@
 import { ContributionHeader, ContributionPaper } from '@components'
 import { Stack } from '@mantine/core'
+import { useUserContext } from '@context'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export const ContributionPage = () => {
+  const { user } = useUserContext()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user?.organization) {
+      navigate('/organization/new')
+    }
+  }, [user, navigate])
+
   return (
     <Stack>
       <ContributionHeader />
