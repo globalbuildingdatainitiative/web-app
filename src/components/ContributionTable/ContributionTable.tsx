4ab1886429b8dc62059988f18d6ee7e5c6c7159a
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-07-09 18:59:22 +0200

    Updated Country Field and Test Files

--- src/components/ContributionTable/ContributionTable.tsx
+++ src/components/ContributionTable/ContributionTable.tsx
@@ -1,69 +1,3 @@
-/*
-import { Contribution, useGetContributionsQuery } from '@queries'
-import { MantineReactTable, MRT_ColumnDef, useMantineReactTable } from 'mantine-react-table'
-import { useMemo } from 'react'
-import dayjs from 'dayjs'
-
-export const ContributionTable = () => {
-  const { loading, error, data } = useGetContributionsQuery()
-
-  const columns = useMemo<MRT_ColumnDef<Pick<Contribution, 'id'>>[]>(
-    () => [
-      {
-        accessorKey: 'id',
-        header: 'ID',
-        size: 50,
-      },
-      {
-        accessorKey: 'uploadedAt',
-        header: 'Date',
-        Cell: ({ cell }) => <>{dayjs(cell.getValue() as string).format('DD/MM/YYYY')}</>,
-        size: 50,
-      },
-      {
-        accessorKey: 'organization.name',
-        header: 'Organization',
-        size: 300,
-      },
-      {
-        accessorKey: 'project.name',
-        header: 'Project',
-        size: 300,
-      },
-    ],
-    [],
-  )
-
-  const rowData = useMemo(() => data?.contributions || [], [data])
-  const totalRowCount = useMemo(() => data?.contributions.length || 0, [data])
-
-  const table = useMantineReactTable({
-    columns,
-    data: rowData,
-    rowCount: totalRowCount,
-    enableColumnActions: false,
-    enableColumnFilters: false,
-    enablePagination: false,
-    enableSorting: false,
-    mantineToolbarAlertBannerProps: error
-      ? {
-          color: 'red',
-          children: error.message,
-        }
-      : undefined,
-    state: {
-      isLoading: loading,
-      showAlertBanner: !!error,
-      showSkeletons: false,
-    },
-  })
-  return (
-    <div data-testid='ContributionTable'>
-      <MantineReactTable table={table} />
-    </div>
-  )
-}
-*/
 import { Contribution, useGetContributionsQuery } from '@queries'
 import { MantineReactTable, MRT_ColumnDef, useMantineReactTable } from 'mantine-react-table'
 import { useEffect, useMemo } from 'react'
@@ -75,7 +9,7 @@ export const ContributionTable = () => {
   useEffect(() => {
     if (data && data.contributions) {
       data.contributions.forEach((contribution) => {
-        console.log('Country:', contribution.project?.location?.country)
+        console.log('Country:', contribution.project?.location?.countryName)
       })
     }
   }, [data])
@@ -99,7 +33,7 @@ export const ContributionTable = () => {
         size: 50,
       },
       {
-        accessorKey: 'project.location.country',
+        accessorKey: 'project.location.countryName',
         header: 'Country',
         size: 100,
       },
