0b0da1e2f00d65d5cc671ac96e2cc613405f1f86
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-07-17 12:42:57 +0200

    updated tests

--- src/__mocks__/data/getContributionsResponse.ts
+++ src/__mocks__/data/getContributionsResponse.ts
@@ -1,16 +1,18 @@
 export default {
   data: {
-    contributions: [
-      {
-        id: 'e3b986a2-e135-42d7-a5fc-0063fde6e9b1',
-        uploadedAt: '2018-04-04T16:00:00.000Z',
-        project: {
-          name: 'My Project',
+    contributions: {
+      items: [
+        {
+          id: 'e3b986a2-e135-42d7-a5fc-0063fde6e9b1',
+          uploadedAt: '2018-04-04T16:00:00.000Z',
+          project: {
+            name: 'My Project',
+          },
+          organization: {
+            name: 'My Organization',
+          },
         },
-        organization: {
-          name: 'My Organization',
-        },
-      },
-    ],
+      ],
+    },
   },
 }
