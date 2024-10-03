972b79abaa09c21f7901e6df7d46af04c4141033
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-10-03 09:12:15 +0200

    Minor Improvements

--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -35,7 +35,7 @@ export const SignOut = ({ collapsed }: SignOutProps) => {
         onClick={onLogout}
       >
         {collapsed ? (
-          <IconLogout size={16} />
+          <IconLogout size={20} />
         ) : isHovered ? (
           <>
             <IconLogout size={16} />
