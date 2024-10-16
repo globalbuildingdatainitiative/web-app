ad421b0ae61a26812b6f9d095295b158679d0fc7
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-10-16 14:48:44 +0200

    Added Leave/Remove Organization Button

--- src/components/MemberTable/MemberTable.tsx
+++ src/components/MemberTable/MemberTable.tsx
@@ -1,12 +1,17 @@
-import { useGetUsersQuery } from '@queries'
+import { useGetUsersQuery, useUpdateUserMutation } from '@queries'
 import { MantineReactTable, MRT_ColumnDef, useMantineReactTable } from 'mantine-react-table'
 import React, { useMemo } from 'react'
+import { useUserContext } from '@context'
+import { Button } from '@mantine/core'
+import { useNavigate } from 'react-router-dom'
+import { theme } from '@components'
 
 interface MemberTableProps {
   organizationId: string
 }
 
 interface Row {
+  id: string
   firstName: string
   lastName: string
   email: string
@@ -15,10 +20,15 @@ interface Row {
 }
 
 export const MemberTable: React.FC<MemberTableProps> = ({ organizationId }) => {
+  const { user: currentUser } = useUserContext()
+  const currentUserId = currentUser?.id
+  const navigate = useNavigate()
+
   const {
     loading: loadingUsers,
     error: errorUsers,
     data: usersData,
+    refetch: refetchUsers,
   } = useGetUsersQuery({
     variables: {
       filters: {
@@ -28,6 +38,60 @@ export const MemberTable: React.FC<MemberTableProps> = ({ organizationId }) => {
       },
     },
   })
+
+  const [updateUser] = useUpdateUserMutation()
+
+  const getUserRole = (userId: string): string => {
+    const user = rowData.find((row) => row.id === userId)
+    return user ? user.role : 'Member'
+  }
+
+  const handleRemoveFromOrganization = async (userId: string) => {
+    try {
+      await updateUser({
+        variables: {
+          userInput: {
+            id: userId,
+            organizationId: null,
+            role: null,
+          },
+        },
+      })
+      if (userId === currentUserId) {
+        navigate('/organization/new')
+      } else {
+        refetchUsers()
+        navigate('/organization')
+      }
+    } catch (error) {
+      console.error('Error removing user from organization:', error)
+    }
+  }
+
+  const formatRole = (role: string | null | undefined): string => {
+    if (!role) return 'N/A'
+    return role.charAt(0).toUpperCase() + role.slice(1).toLowerCase()
+  }
+
+  const rowData = useMemo(() => {
+    if (!usersData) return []
+
+    return usersData.users
+      .filter((user) => {
+        const status = user.inviteStatus?.toLowerCase()
+        return !user.invited || status === 'accepted'
+      })
+      .map((user) => ({
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        timeJoined: user.timeJoined,
+        role: formatRole(user.role),
+      })) as Row[]
+  }, [usersData])
+
+  const currentUserRole = getUserRole(currentUserId)
 
   const columns = useMemo<MRT_ColumnDef<Row>[]>(
     () => [
@@ -53,31 +117,23 @@ export const MemberTable: React.FC<MemberTableProps> = ({ organizationId }) => {
         header: 'Role',
         size: 100,
       },
+      {
+        accessorKey: 'action',
+        header: '',
+        Cell: ({ row }) => (
+          <Button
+            onClick={() => handleRemoveFromOrganization(row.original.id)}
+            disabled={currentUserRole !== 'Owner' && row.original.id !== currentUserId}
+            color={row.original.id === currentUserId ? theme?.colors?.green?.[9] : theme?.colors?.red?.[7]}
+          >
+            {row.original.id === currentUserId ? 'Leave Organization' : 'Remove from Organization'}
+          </Button>
+        ),
+        size: 200,
+      },
     ],
-    [],
+    [currentUserId, currentUserRole],
   )
-
-  const formatRole = (role: string | null | undefined): string => {
-    if (!role) return 'N/A'
-    return role.charAt(0).toUpperCase() + role.slice(1).toLowerCase()
-  }
-
-  const rowData = useMemo(() => {
-    if (!usersData) return []
-
-    return usersData.users
-      .filter((user) => {
-        const status = user.inviteStatus?.toLowerCase()
-        return !user.invited || status === 'accepted'
-      })
-      .map((user) => ({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        timeJoined: user.timeJoined,
-        role: formatRole(user.role),
-      })) as Row[]
-  }, [usersData])
 
   const table = useMantineReactTable({
     columns,
