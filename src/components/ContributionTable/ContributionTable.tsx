4593815c42ab7df86540e2a2c231c36286c6a90b
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-10-31 11:52:20 +0100

    Adjusted Position of public column

--- src/components/ContributionTable/ContributionTable.tsx
+++ src/components/ContributionTable/ContributionTable.tsx
@@ -89,14 +89,14 @@ export const ContributionTable: React.FC = () => {
         },
       },
       {
-        accessorKey: 'public',
-        header: 'Public',
-        Cell: ({ cell }) => <Text>{cell.getValue<boolean>() ? 'Yes' : 'No'}</Text>,
-      },
-      {
         accessorKey: 'uploadedAt',
         header: 'Date',
         Cell: ({ cell }) => <Text>{dayjs(cell.getValue<string>()).format('DD/MM/YYYY')}</Text>,
+      },
+      {
+        accessorKey: 'public',
+        header: 'Public',
+        Cell: ({ cell }) => <Text>{cell.getValue<boolean>() ? 'Yes' : 'No'}</Text>,
       },
       {
         accessorKey: 'project.lifeCycleStages',
