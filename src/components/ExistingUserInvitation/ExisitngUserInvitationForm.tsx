a0a1ddc3d851f00410e184c0d097fc9f6c720458
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2024-11-05 11:57:37 +0100

    Moved Render to different component

---
+++ src/components/ExistingUserInvitation/ExisitngUserInvitationForm.tsx
@@ -0,0 +1,18 @@
+import { Button, Stack, Text } from '@mantine/core'
+import { ErrorMessage } from '@components'
+
+interface ExistingUserInvitationFormProps {
+  onAccept: () => void
+  loading: boolean
+  error: Error | null
+}
+
+export const ExistingUserInvitationForm = ({ onAccept, loading, error }: ExistingUserInvitationFormProps) => (
+  <Stack align='center'>
+    <Text ta='center'>Click the button below to accept the invitation and sign in:</Text>
+    {error && <ErrorMessage error={error} />}
+    <Button radius='lg' px={16} size='md' w={500} onClick={onAccept} loading={loading} disabled={loading}>
+      Accept Invitation and Sign In
+    </Button>
+  </Stack>
+)
