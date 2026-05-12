e6cac7f4e477d46c9f46bd9f315a8a77f9cf7d6f
cy-len <cyprien.lengagne@gmail.com>
2026-05-12 09:11:08 +0200

    fix: added banner

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -3,6 +3,7 @@ import { useMediaQuery } from '@mantine/hooks'
 import { ErrorBoundary, Greeting, SidePanel } from '@components'
 import { Outlet } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { V2Banner } from 'components/V2Banner/V2Banner'
 
 export const AppLayout = () => {
   const theme = useMantineTheme()
@@ -37,6 +38,7 @@ export const AppLayout = () => {
     >
       <AppShell.Header pl={`calc(${navbarWidth}px + var(--mantine-spacing-xl))`} bg={theme.other.backgroundColor}>
         <ErrorBoundary>
+          <V2Banner />
           <Greeting />
         </ErrorBoundary>
       </AppShell.Header>
